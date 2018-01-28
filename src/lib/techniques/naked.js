bea37ebfcc3acff554330d2cab88492cfdca005e
PolyDevil <polydevil@cocaine.ninja>
2018-01-29 00:50:07 +0300

    Fix "Naked" technique

--- src/lib/techniques/naked.js
+++ src/lib/techniques/naked.js
@@ -1,10 +1,15 @@
-import HiddenNaked from './hidden_naked'
-import { TUPLE } from '../consts'
-import { AddToSetInMap, GetSubsetSizes, GetTypeOfCells } from '../utils'
+import { TUPLE, MIN_SUBSET_SIZE, MAX_SUBSET_SIZE, DEFAULT_SUBSET_SIZE } from '../consts'
+import { GetIds } from '../unit'
+import { AddToSetInMap, GetSubsetSizes, GetTypeOfCells, GetCombinations } from '../utils'
 
-export default class $Naked extends HiddenNaked {
+export default class $Naked {
   constructor(grid, unsolved) {
-    super(grid, unsolved);
+    this.grid = grid;
+    this.unsolved = unsolved;
+
+    this.rows = new Map();
+    this.columns = new Map();
+    this.blocks = new Map();
     this.name = 'Naked';
   }
 
@@ -59,4 +64,62 @@ export default class $Naked extends HiddenNaked {
     });
   }
 
+  resetUnitsStore() {
+    this.rows.clear();
+    this.columns.clear();
+    this.blocks.clear();
+  }
+
+  scan($maxSubsetSize = DEFAULT_SUBSET_SIZE) {
+    this.resetUnitsStore();
+
+    const { unsolved, name, rows, columns, blocks } = this;
+
+    unsolved.forEach(id => {
+      const [row, column, block] = GetIds(id);
+      AddToSetInMap(rows, row, id);
+      AddToSetInMap(columns, column, id);
+      AddToSetInMap(blocks, block, id);
+    });
+
+    if (Number.isInteger($maxSubsetSize)) {
+      if ($maxSubsetSize < MIN_SUBSET_SIZE) {
+        throw new Error(`Error! $maxSubsetSize should be greater or equal than ${MIN_SUBSET_SIZE}.`);
+      } else {
+        const subsetSize = Math.min($maxSubsetSize, MAX_SUBSET_SIZE);
+        this.analyze(subsetSize, rows, columns, blocks);
+      }
+    } else {
+      throw new Error(`Error! $maxSubsetSize should be an integer. Check ${name}:scan: method.`);
+    }
+  }
+
+  evaluate($subsetSizes, $subsets) {
+    $subsetSizes.reduce((previousValue, currentValue) => {
+      const subset = $subsets.get(currentValue);
+      if (subset) {
+        const { size } = subset;
+        if (size > 0) {
+          const set = new Map([...previousValue, ...subset]);
+          if (set.size > currentValue - 1) {
+            this.solve(currentValue, set);
+          }
+          return set;
+        }
+      }
+      return previousValue;
+    }, new Map());
+  }
+
+  solve($type, $map) {
+    const groups = GetCombinations([...$map.keys()], $type);
+    groups.forEach(group => {
+      const candidates = group.map(el => $map.get(el));
+      const union = new Set(candidates.reduce((p, c) => p.concat(c), []));
+      if (union.size === $type) {
+        // console.log('eliminate', union, group, $type);
+        this.eliminate(union, group, $type);
+      }
+    });
+  }
 }
